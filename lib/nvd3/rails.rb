8fd21eb938981b3799fc0db9a55c4cf9e03200f1
Dale Stevens <voltechs@gmail.com>
2014-02-13 10:04:32 -0800

    1.1.15b

---
+++ lib/nvd3/rails.rb
@@ -0,0 +1,7 @@
+require "nvd3/rails/version"
+
+module NVD3
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/nvd3/rails/version.rb
@@ -0,0 +1,5 @@
+module NVD3
+  module Rails
+    VERSION = "1.1.15b"
+  end
+end
